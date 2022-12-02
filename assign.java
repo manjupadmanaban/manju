aedc56ebfd4bdbf16b60317730e0cfde5972cceb
manjupadmanaban <119653406+manjupadmanaban@users.noreply.github.com>
2022-12-02 12:26:45 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,22 @@
+package assighnment;
+
+public class assign {
+
+	public static void main(String[] args) {
+		int n=13;
+		for(int i=0;i<=n;i++)
+		{
+		
+			for(int j=0;j<=n;j++)
+			{
+				if(i==n||i==n-1||i-j>=(n-1)-(n-1)/2||i+j>=n+(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+		}
+		System.out.println();
+		}
+
+	}
+
+}
---
+++ assign1.java
@@ -0,0 +1,22 @@
+package assighnment;
+
+public class assign1 {
+
+	public static void main(String[] args) {
+		int n=7;
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				if(i==j)
+					System.out.print("INEURON");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
